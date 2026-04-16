5660a789495a2da72e8dcef485315370d0c838fa
git-zxrcodev <zxrcodev@icloud.com>
2026-04-16 19:05:29 +0200

    hh

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,18 @@
 import type { NextConfig } from 'next'
 
+const isProd = process.env.NODE_ENV === 'production'
+const repoName = 'portfolio-next1'
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
   images: {
     formats: ['image/avif', 'image/webp'],
+    unoptimized: true,
   },
+  trailingSlash: true,
 }
 
 export default nextConfig
